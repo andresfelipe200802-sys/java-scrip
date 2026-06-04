241e1177761b964b3d9dcfc391271cbed8296bfa
Andres felipe <andresfelipe.200802@gmail.com>
2026-06-04 17:10:00 -0500

    clase 2 de js

---
+++ cap1.Introducción/ex04/ejercicio4.js
@@ -0,0 +1,19 @@
+let nombre, edad, direccion, movil, correo;
+
+nombre = prompt("digite su nombre: ");/* se piden datos al usuario */
+edad = prompt("digite su edad: ");
+direccion = prompt("digite su direccion: ");
+movil = prompt("digite su movil: ");
+correo = prompt("digite su correo: ");
+
+console.log("tu nombre es: ", nombre);
+console.log("tu edad es: ", edad);
+console.log("tu direccion es: ", direccion);
+console.log("tu movil es: ", movil);
+console.log("tu correo es: ", correo);
+
+document.write("tu nombre es: ", nombre);
+document.write("tu edad es: ", edad);
+document.write("tu direccion es: ", direccion);
+document.write("tu movil es: ", movil);
+document.write("tu correo es: ", correo);
---
+++ cap1.Introducción/ex05/ejercicio5.js
@@ -0,0 +1,24 @@
+let a, b;
+let c, d ;
+let suma, resta, mult, division, residuo, potencia;
+a= parseFloat(prompt("ingresa el primer numero: ")) ;
+b= parseFloat(prompt("ingresa el segundo numero: "));
+
+
+/* resultados de operaciones */
+suma = a + b;
+resta = a-b;
+mult = a*b;
+division = a/b;
+potencia = a**b;
+
+/* imprimir resultados; */
+document.write("la suma es: ", suma, "<br>");
+document.write("la resta es: ", resta, "<br>");
+document.write("la mult es: ", mult, "<br>");
+document.write("la division es: ", division, "<br>");
+document.write("la potencia es: ", potencia, "<br>");
+
+
+
+
